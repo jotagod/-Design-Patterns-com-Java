dc19d59b1b7e5b7599af6ae1ae1f3ce4c554dc2c
João Aurélio <joao_mcd@hotmail.com>
2024-07-27 11:05:04 -0300

    Strategy implementado

--- singleton/lab-padroes-projeto-java/src/one/digitalinnovation/gof/Test.java
+++ singleton/lab-padroes-projeto-java/src/one/digitalinnovation/gof/Test.java
@@ -1,5 +1,14 @@
 package one.digitalinnovation.gof;
 
+import one.digitalinnovation.gof.singleton.SingletonEager;
+import one.digitalinnovation.gof.singleton.SingletonLazy;
+import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
+import one.digitalinnovation.gof.strategy.Comportamento;
+import one.digitalinnovation.gof.strategy.ComportamentoAgressivo;
+import one.digitalinnovation.gof.strategy.ComportamentoDefensivo;
+import one.digitalinnovation.gof.strategy.ComportamentoNormal;
+import one.digitalinnovation.gof.strategy.Robo;
+
 public class Test {
     public static void main(String[] args) {
 
@@ -19,6 +28,28 @@ public class Test {
         System.out.println(lazyHolder);
         lazyHolder = SingletonLazyHolder.getInstancia();
         System.out.println(lazyHolder);
+
+        // Strategy
+
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+
+        robo.mover();
+        robo.mover();
+        
+        robo.setComportamento(defensivo);
+
+        robo.mover();
+
+        robo.setComportamento(agressivo);
+
+        robo.mover();
+        robo.mover();
+        robo.mover();
     }
 
 }
